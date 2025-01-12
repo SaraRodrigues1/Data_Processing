61fabdfd920e7d02eb425014b9c721e12329f972
Sara Rodrigues Cardoso <sara.rodrigues.cardoso@student.nhlstenden.com>
2025-01-12 21:58:50 +0100

    Add: dependencies

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const express= require('express');
 const app = express; 
 
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const secretKey = 'secret-key';
+
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
@@ -9,7 +14,9 @@ server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
- // test
- app.get('/', (req, res) => {
-    res.send('Hello, Express!');
-  });
+// test
+app.get('/', (req, res) => {
+   res.send('Hello, Express!');
+});
+
+require('./app/routes')(app, {});
