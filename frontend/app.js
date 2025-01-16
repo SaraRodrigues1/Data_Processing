1e1aab97aefd37a721f68b9bffafb269f3aad190
Iarina <112045565+iarinamd@users.noreply.github.com>
2025-01-16 16:19:09 +0100

    Add the frontend

---
+++ frontend/app.js
@@ -0,0 +1,9 @@
+console.log("Netflix Clone Loaded!");
+
+//Add Profile Functionality
+document.addEventListener("DOMContentLoaded", () => {
+  const addProfileButton = document.getElementById("addProfile");
+  addProfileButton.addEventListener("click", () => {
+    alert("Add Profile functionality coming soon!");
+  });
+});
